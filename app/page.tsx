65441df76abf2e25905e7357be55cb1282d1f86e
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2023-11-17 17:57:50 +0100

    Basic Github Authentification Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { getAuthSession } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return (
-    <div className="m-4">
+    <div className="">
       <Button>Click Me!</Button>
       <Input />
     </div>
